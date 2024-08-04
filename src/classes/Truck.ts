386dd8a748f4b3854ffa1486dc651fa700c5901e
kc-claas <keithclaas@gmail.com>
2024-08-04 15:19:52 -0500

    removed console.log test entries

--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -60,8 +60,6 @@ class Truck extends Vehicle implements AbleToTow{
     // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
     // TODO: If it is, log that the vehicle is being towed
     // TODO: If it is not, log that the vehicle is too heavy to be towed
-      console.log(vehicle.weight)
-      console.log(this.towingCapacity)
       if (vehicle.weight <= this.towingCapacity) {
         console.log(`${vehicle.make} ${vehicle.model} is being towed.`)
       }
